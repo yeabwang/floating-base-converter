e6f42eb787e25f6ecbc405cd6fd4b7537516c3c0
yeabwang <yeabsiratesfaye781@gmail.com>
2025-08-05 15:37:29 +0800

    Add initial project files and setup for floating-base-converter package

---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="floating-base-converter",
+    version="1.0.0",
+    author="Yeabwang",
+    author_email="wangxiayu@yeab.io",
+    description="A Python package for converting floating-point numbers between different bases",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/floating-base-converter",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.7",
+    install_requires=[],
+    extras_require={
+        "dev": [
+            "pytest>=6.0",
+            "pytest-cov>=2.0",
+            "black>=21.0",
+            "flake8>=3.8",
+        ],
+    },
+    entry_points={
+        "console_scripts": [
+            "base-converter=base_converter.cli:main",
+        ],
+    },
+    keywords="base conversion, floating point, binary, octal, hexadecimal, decimal",
+)
