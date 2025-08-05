6bc6fbc6f7b9e28f5de7fe1322e7e63b445723c4
yeabwang <yeabsiratesfaye781@gmail.com>
2025-08-05 15:38:46 +0800

    Add initial implementation of Floating Base Converter package

---
+++ base_converter/__init__.py
@@ -0,0 +1,26 @@
+"""
+Floating Base Converter
+
+A Python package for converting floating-point numbers between different bases
+(decimal, binary, octal, hexadecimal).
+
+Example:
+    >>> from base_converter import BaseConverter
+    >>> converter = BaseConverter()
+    >>> result = converter.decimal_to_hex(3.14159, precision=6)
+    >>> print(result)  # "3.243F6A"
+"""
+
+from .converter import BaseConverter, ConversionError
+from .utils import validate_input, normalize_input
+
+__version__ = "1.0.0"
+__author__ = "Yeabwang"
+__email__ = "wangxiayu@yeab.io"
+
+__all__ = [
+    "BaseConverter",
+    "ConversionError",
+    "validate_input",
+    "normalize_input",
+]
